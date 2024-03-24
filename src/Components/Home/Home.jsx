84f0b1413ec41458edeb31f622833381749b1799
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-24 19:22:27 +0530

    Create Home.jsx

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+
+function Home() {
+  return (
+    <>
+      <Layout>
+        Home
+      </Layout>
+    </>
+  );
+}
+
+export default Home;
