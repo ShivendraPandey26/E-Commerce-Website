bade3b77a143a1f3e1fb2bda221de5cfae4a3e21
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-24 16:13:03 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-900">
+        cd "E-Commerce Website"
+      </h1>
+    </>
+  );
+}
+
+export default App;
