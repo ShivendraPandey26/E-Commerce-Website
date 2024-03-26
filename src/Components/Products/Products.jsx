664075be91103fefd935d4a96d2d02da9a6f745a
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-26 23:23:10 +0530

    Create Products.jsx

---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+
+function Products() {
+  return (
+    <Layout>
+        <div>
+        Products Page
+        </div>
+    </Layout>
+  )
+}
+
+export default Products
