c234a89403ac21f22a83b9279d6d8193551844a1
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-26 23:23:15 +0530

    Update Layout.jsx

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
+import Footer from '../Footer/Footer'; 
 
 function Layout({ children }) {
   return (
     <>
         <Navbar />
-        <div className='content'>
+        <div className=' content '>
             {children}
         </div>
         <Footer />
