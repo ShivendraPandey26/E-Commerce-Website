e5c1d1d09c2d2ddd748ad32756561a691ab083e8
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-26 23:23:18 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,131 +1,150 @@
 import React from "react";
 import { FaShopify, FaTwitter, FaInstagram } from "react-icons/fa";
-import { FaFacebook, FaLinkedin, FaQuora } from "react-icons/fa6";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaQuora,
+  FaRegCopyright,
+} from "react-icons/fa6";
 
 function Footer() {
   return (
-    <div className="flex items-center justify-center bg-black p-10  ">
-      <div className="m-5 max-w-5xl">
-        <div className="flex flex-col items-center justify-around md:flex-row">
-          <div className="items-center m-1 me-5 justify-items-start w-60">
-            <FaShopify className="text-white text-7xl mx-auto" />
-            <div className="text-gray-500 md:text-center">
-              {" "}
-              Explore SnapShop for all your shopping needs. Secure checkout,
-              fast delivery. Happy shopping!
+    <>
+      <div className="flex items-center justify-evenly bg-black p-10  ">
+        <div className="m-5 max-w-5xl">
+          <div className="flex flex-col items-center justify-around md:flex-row">
+            <div className="items-center m-1 me-5 justify-items-start w-60">
+              <FaShopify
+                className="text-white text-7xl mx-auto cursor-pointer"
+                onClick={() =>
+                  window.open("https://github.com/ShivendraPandey26/", "_blank")
+                }
+              />
+              <div className="text-gray-300 md:text-center">
+                {" "}
+                Explore SnapShop for all your shopping needs. Secure checkout,
+                fast delivery. Happy shopping!
+              </div>
             </div>
-          </div>
-          <div className="items-center m-1 justify-items-start w-60">
-            <h1 className="flex flex-col text-xl text-blue-600 font-medium">
-              {" "}
-              Product{" "}
-            </h1>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Home
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Shop
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              About
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Contact
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Chrome Extensions
-            </a>
-          </div>
-          <div className="items-center m-1 justify-start w-60">
-            <h1 className="flex flex-col text-xl text-blue-600 font-medium">
-              {" "}
-              Company{" "}
-            </h1>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Terms
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Customers
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Blog
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Terms of Services
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Privacy Policy
-            </a>
-          </div>
-          <div className="items-center m-1 w-60">
-            <h1 className="flex flex-col text-xl text-blue-600 font-medium">
-              {" "}
-              Support{" "}
-            </h1>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Community
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Support Customers
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Documentations
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Service Providers
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              Email Contact
-            </a>
-          </div>
-          <div className="items-center mt-10 pt-5 w-60 md:-mt-16">
-            <h1 className="flex flex-col text-xl text-blue-600 font-medium">
-              {" "}
-              Contact Us{" "}
-            </h1>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              suppport@admin.com
-            </a>
-            <a href="#" className="flex hover:text-blue-600 text-gray-500">
-              (+251)987-6543-210
-            </a>
+            <div className="items-center m-1 justify-items-start w-60  lg:mt-0 mt-10">
+              <h1 className="flex flex-col text-xl text-blue-600 font-medium">
+                {" "}
+                Product{" "}
+              </h1>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Home
+              </a>
+              
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Shop
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                About
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Contact
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Chrome Extensions
+              </a>
+            </div>
+            <div className="items-center m-1 justify-start w-60 lg:mt-0 mt-10">
+              <h1 className="flex flex-col text-xl text-blue-600 font-medium">
+                {" "}
+                Company{" "}
+              </h1>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Terms
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Customers
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Blog
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Terms of Services
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear">
+                Privacy Policy
+              </a>
+            </div>
+            <div className="items-center m-1 w-60 lg:mt-0 mt-10">
+              <h1 className="flex flex-col text-xl text-blue-600 font-medium">
+                {" "}
+                Support{" "}
+              </h1>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear ">
+                Community
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear ">
+                Support Customers
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear ">
+                Documentations
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear ">
+                Service Providers
+              </a>
+              <a href="#" className="flex hover:text-blue-600 text-gray-500 hover:ps-2 duration-200 ease-linear ">
+                Email Contact
+              </a>
+            </div>
+            <div className="items-center w-60 lg:mb-10 pt-10">
+              <h1 className="flex flex-col text-xl text-blue-600 font-medium ">
+                {" "}
+                Contact Us{" "}
+              </h1>
+              <a href="#" className="flex text-gray-500 hover:ps-2 hover:text-blue-600 duration-200 ease-linear">
+                suppport@admin.com
+              </a>
+              <a href="#" className="flex text-gray-500 hover:ps-2 hover:text-blue-600 duration-200 ease-linear">
+                (+251)987-6543-210
+              </a>
 
-            <div className="flex">
-              <a
-                href="#"
-                className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
-              >
-                <FaTwitter className="text-xl" />
-              </a>
-              <a
-                href="#"
-                className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
-              >
-                <FaInstagram className="text-xl" />
-              </a>
-              <a
-                href="#"
-                className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
-              >
-                <FaFacebook className="text-xl" />
-              </a>
-              <a
-                href="#"
-                className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
-              >
-                <FaLinkedin className="text-xl" />
-              </a>
-              <a
-                href="#"
-                className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
-              >
-                <FaQuora className="text-xl" />
-              </a>
+              <div className="flex">
+                <a
+                  href="#"
+                  className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
+                >
+                  <FaTwitter className="text-xl hover:scale-150 transition-all ease-in-out" />
+                </a>
+                <a
+                  href="#"
+                  className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
+                >
+                  <FaInstagram className="text-xl hover:scale-150 transition-all ease-in-out" />
+                </a>
+                <a
+                  href="#"
+                  className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
+                >
+                  <FaFacebook className="text-xl hover:scale-150 transition-all ease-in-out" />
+                </a>
+                <a
+                  href="#"
+                  className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
+                >
+                  <FaLinkedin className="text-xl hover:scale-150 transition-all ease-in-out" />
+                </a>
+                <a
+                  href="#"
+                  className="w-9 h-9 rounded-full text-white hover:text-blue-600 pt-7 inline-block"
+                >
+                  <FaQuora className="text-xl hover:scale-150 transition-all ease-in-out" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="w-full sm:flex sm:items-center sm:justify-between">
+        <center className= "w-full text-white bg-black flex items-center justify-center font-medium py-4"> 
+          <span><FaRegCopyright className="me-2" />   </span> Copyright {new Date().getFullYear()} All Rights
+          Reserved.  Developed By <a href="https://github.com/ShivendraPandey26/ , '_blank" className="underline underline-offset-4 ms-2"> Shivendra Pandey</a>.
+        </center>
+      </div>
+    </>
   );
 }
 
