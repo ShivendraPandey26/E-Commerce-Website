625d843abad24b774778a9033b2a23efd7b84d5d
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-29 02:56:25 +0530

    Update ImageSlider.jsx

--- src/Components/Home/ImageSlider.jsx
+++ src/Components/Home/ImageSlider.jsx
@@ -1,38 +1,49 @@
 import React from "react";
 import { TECarousel, TECarouselItem } from "tw-elements-react";
+import './ImageSlider.css'
 
 export default function CarouselWithIndicators() {
   return (
     <>
-      <TECarousel showControls showIndicators ride="carousel">
+      <TECarousel showControls showIndicators ride="carousel" interval={3000}>
         <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
           <TECarouselItem
             itemID={1}
-            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="bannerImage relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
           >
             <img
               src="./src/assets/banner-1.jpg"
-              className="block w-full h-[87vh] bg-cover "
+              className="w-full h-full object-cover"
               alt="..."
             />
           </TECarouselItem>
           <TECarouselItem
             itemID={2}
-            className="relative float-left hidden -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="bannerImage relative float-left hidden -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
           >
             <img
               src="./src/assets/banner-2.jpg"
-              className="block w-full h-[87vh] bg-cover "
+              className="w-full h-full object-cover"
               alt="..."
             />
           </TECarouselItem>
           <TECarouselItem
             itemID={3}
-            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="bannerImage relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
           >
             <img
               src="./src/assets/banner-3.jpg"
-              className="block w-full h-[87vh] bg-cover "
+              className="w-full h-full object-cover"
+              alt="..."
+            />
+          </TECarouselItem>
+          <TECarouselItem
+            itemID={4}
+            className="bannerImage relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+          >
+            <img
+              src="./src/assets/banner-4.jpg"
+              className="w-full h-full object-cover"
               alt="..."
             />
           </TECarouselItem>
