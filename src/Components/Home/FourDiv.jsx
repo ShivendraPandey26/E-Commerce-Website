83771af9aced06b2d7e39e400651d714e3801655
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-29 02:56:32 +0530

    Update FourDiv.jsx

--- src/Components/Home/FourDiv.jsx
+++ src/Components/Home/FourDiv.jsx
@@ -6,20 +6,36 @@ import { FiShoppingCart } from "react-icons/fi";
 function FourDiv() {
     return (
         <>
-            <div className="container w-screen h-36 flex items-center justify-around text-white ">
-                <div className="bg-orange-500 p-6 rounded-md hover:bg-orange-600 transition duration-200">
-                    <center> <FaTruckFast size={25} /> </center> FREE SHIPPING
+            <center className="container w-screen h-auto p-14 flex items-center justify-around text-white flex-wrap mx-auto gap-10 my-10">
+                <div className="bg-orange-500 lg:w-[20%] lg:h-20 md:w-[20%] md:h-20 sm:w-[50%] sm:h-20 w-[100%] h-20 p-4  rounded-md hover:bg-orange-600 hover:scale-110 transition duration-300">
+                    <center>
+                        {" "}
+                        <FaTruckFast size={25} />{" "}
+                    </center>{" "}
+                    FREE SHIPPING
                 </div>
-                <div className="bg-orange-500 p-6 rounded-md hover:bg-orange-600 transition duration-200">
-                    <center> <TbTruckReturn size={25}/>  </center> EASSY RETURN
+                <div className="bg-orange-500  lg:h-20 md:w-[20%] md:h-20 sm:w-[50%] sm:h-20 w-[100%] h-20 p-4  rounded-md hover:bg-orange-600 hover:scale-110 transition duration-300">
+                    <center>
+                        {" "}
+                        <TbTruckReturn size={25} />{" "}
+                    </center>{" "}
+                    EASSY RETURN
                 </div>
-                <div className="bg-orange-500 p-6 rounded-md hover:bg-orange-600 transition duration-200">
-                    <center> <FaPaypal size={25}/> </center>PAYMENT SECURED
+                <div className="bg-orange-500 lg:h-20 md:w-[20%] md:h-20 sm:w-[50%] sm:h-20 w-[100%] h-20 p-4   rounded-md hover:bg-orange-600 hover:scale-110 transition duration-300">
+                    <center>
+                        {" "}
+                        <FaPaypal size={25} />{" "}
+                    </center>
+                    PAYMENT SECURED
                 </div>
-                <div className="bg-orange-500 p-6 rounded-md hover:bg-orange-600 transition duration-200">
-                    <center> <FiShoppingCart size={25}/> </center>AUTHENTI PRODUCTS
+                <div className="bg-orange-500  lg:h-20 md:w-[20%] md:h-20 sm:w-[50%] sm:h-20 w-[100%] h-20 p-4   rounded-md hover:bg-orange-600 hover:scale-110 transition duration-300">
+                    <center>
+                        {" "}
+                        <FiShoppingCart size={25} />{" "}
+                    </center>
+                    AUTHENTI PRODUCTS
                 </div>
-            </div>
+            </center>
         </>
     );
 }
