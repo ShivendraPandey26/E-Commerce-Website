48f390f3b6b2654b2a033b84229c7c77440c4351
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-29 02:56:40 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,45 +9,51 @@ function About() {
           <h2 className="text-5xl font-semibold mb-10 text-[#31587c] font-serif">
             About Us
           </h2>
-          <p className="text-lg text-[#32597d] font-sans mb-4 leading-relaxed w-1/2 mx-auto">
+          <p className="text-lg text-[#32597d] font-sans mb-4 leading-relaxed md:w-[50%] w-[100%] mx-auto">
             Welcome to our e-commerce store! We are a family-owned business that
             has been selling home decor items for over 20 years. Our passion for
             unique and beautiful items has led us to create this online store,
             where we can share our collection with a wider audience.
           </p>
-          <h2 className="text-left text-2xl font-serif font-bold text-orange-600 my-10">Our Story</h2>
+          <h2 className="text-left text-2xl font-serif font-bold text-orange-600 my-10">
+            Our Story
+          </h2>
 
           <div className="lg:flex mb-10">
+            <div>
+              <img
+                src="./src/assets/about-01.jpg"
+                alt="about image"
+                height={300}
+              />
+            </div>
 
-          <div>
-          <img src="./src/assets/about-01.jpg" alt="about image" height= {300} />
-          </div>
-
-          <p className="text-xl text-[#32597d] font-medium mb-9 p-5 text-start leading-relaxed">
-            We believe that every home should be a reflection of the people who
-            live there. That's why we offer a diverse range of products, from
-            vintage furniture to modern art prints, that cater to all styles and
-            budgets. Our products are carefully curated to ensure that they are
-            of the highest quality and meet our customers' expectations.
-          </p>
-
+            <p className="text-xl text-[#32597d] font-medium mb-9 p-5 text-start leading-relaxed">
+              We believe that every home should be a reflection of the people
+              who live there. That's why we offer a diverse range of products,
+              from vintage furniture to modern art prints, that cater to all
+              styles and budgets. Our products are carefully curated to ensure
+              that they are of the highest quality and meet our customers'
+              expectations.
+            </p>
           </div>
 
           <div className="lg:flex mb-10">
+            <p className="text-xl text-[#32597d] font-medium mb-9 p-5 text-start leading-relaxed">
+              We pride ourselves on our excellent customer service and are
+              committed to making every shopping experience a positive one. Our
+              team is always available to answer any questions you may have and
+              to help you find the perfect item for your home.
+            </p>
 
-          <p className="text-xl text-[#32597d] font-medium mb-9 p-5 text-start leading-relaxed">
-            We pride ourselves on our excellent customer service and are
-            committed to making every shopping experience a positive one. Our
-            team is always available to answer any questions you may have and to
-            help you find the perfect item for your home.
-          </p>
-
-          <div>
-          <img src="./src/assets/about-02.jpg" alt="about image" height= {300} />
+            <div>
+              <img
+                src="./src/assets/about-02.jpg"
+                alt="about image"
+                height={300}
+              />
+            </div>
           </div>
-
-          </div>
-
 
           <p className="text-xl text-[#32597d] font-medium p-5 leading-relaxed">
             Thank you for choosing our e-commerce store. We hope you enjoy
@@ -55,7 +61,8 @@ function About() {
             home.
           </p>
           <p className="text-xl text-[#32597d] font-medium mb-9 p-5 leading-relaxed">
-            - The Team at <span className="text-xl font-semibold text-blue-600">
+            - The Team at{" "}
+            <span className="text-xl font-semibold text-blue-600">
               <span className="text-2xl font-semibold text-orange-600">
                 Snap
               </span>
