6c5d74e8c11becc651b26c45eae8ef2e3b4a68c4
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-29 02:56:16 +0530

    Update PopularProducts.jsx

--- src/Components/Home/PopularProducts.jsx
+++ src/Components/Home/PopularProducts.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React from "react";
+import { TERipple } from "tw-elements-react";
 
 function PopularProducts() {
   return (
-    <div>PopularProducts</div>
-  )
+
+    <div className="p-3">
+
+      <h2 className="text-4xl font-semibold mb-20 text-[#31587c] font-serif text-center underline underline-offset-8">
+        Popular Products
+      </h2>
+
+
+      <div className=" h-full sm:flex items-center justify-center gap-10 m-5">
+
+        {/* first card  */}
+        <span className="bg-white rounded-lg hover:scale-110 duration-300 ease-in">
+          <TERipple>
+            <div className="relative overflow-hidden bg-cover bg-no-repeat ">
+              <img
+                className="rounded-t-lg"
+                src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
+                alt=""
+              />
+              <a href="#!">
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </a>
+            </div>
+          </TERipple>
+          <div className="p-6 bg-white lg:mb-0 md:mb-0 sm:mb-10 mb-10">
+            <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+              Card title
+            </h5>
+            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              Price : 20$
+            </p>
+            <TERipple>
+              <button
+                type="button"
+                className="inline-block rounded bg-orange-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-orange-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Add To Card
+              </button>
+            </TERipple>
+          </div>
+        </span>
+
+        {/* second card  */}
+        <span className="bg-white rounded-lg hover:scale-110 duration-300 ease-in">
+          <TERipple>
+            <div className="relative overflow-hidden bg-cover bg-no-repeat">
+              <img
+                className="rounded-t-lg"
+                src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
+                alt=""
+              />
+              <a href="#!">
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </a>
+            </div>
+          </TERipple>
+          <div className="p-6 bg-white lg:mb-0 md:mb-0 sm:mb-10 mb-10">
+            <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+              Card title
+            </h5>
+            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              Price : 20$
+            </p>
+            <TERipple>
+              <button
+                type="button"
+                className="inline-block rounded bg-orange-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-orange-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Add To Card
+              </button>
+            </TERipple>
+          </div>
+        </span>
+
+        {/* third card  */}
+        <span className="bg-white rounded-lg hover:scale-110 duration-300 ease-in">
+          <TERipple>
+            <div className="relative overflow-hidden bg-cover bg-no-repeat">
+              <img
+                className="rounded-t-lg"
+                src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
+                alt=""
+              />
+              <a href="#!">
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </a>
+            </div>
+          </TERipple>
+          <div className="p-6 bg-white lg:mb-0 md:mb-0 sm:mb-10 mb-10">
+            <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+              Card title
+            </h5>
+            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              Price : 20$
+            </p>
+            <TERipple>
+              <button
+                type="button"
+                className="inline-block rounded bg-orange-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-orange-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Add To Card
+              </button>
+            </TERipple>
+          </div>
+        </span>
+
+
+        {/* fourth card  */}
+        <span className="bg-white rounded-lg hover:scale-110 duration-300 ease-in">
+          <TERipple>
+            <div className="relative overflow-hidden bg-cover bg-no-repeat">
+              <img
+                className="rounded-t-lg"
+                src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
+                alt=""
+              />
+              <a href="#!">
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </a>
+            </div>
+          </TERipple>
+          <div className="p-6 bg-white lg:mb-0 md:mb-0 sm:mb-10 mb-10">
+            <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+              Card title
+            </h5>
+            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              Price : 20$
+            </p>
+            <TERipple>
+              <button
+                type="button"
+                className="inline-block rounded bg-orange-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-orange-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Add To Card
+              </button>
+            </TERipple>
+          </div>
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default PopularProducts
+export default PopularProducts;
