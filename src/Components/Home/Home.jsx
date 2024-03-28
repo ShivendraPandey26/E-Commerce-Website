a73ee18608126533dbd0b678502a64ece16cee5f
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-28 13:25:55 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Layout from "../Layout/Layout";
+import ImageSlider from "./ImageSlider";
+import FourDiv from "./FourDiv";
+import PopularProducts from "./PopularProducts";
 
 function Home() {
   return (
     <>
       <Layout>
-        Home
+        {/* Home page start */}
+        <div className="bg-[#eaf5f5]">
+          <ImageSlider />
+          <FourDiv />
+          <PopularProducts />
+        </div>
       </Layout>
     </>
   );
