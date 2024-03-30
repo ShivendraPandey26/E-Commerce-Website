dcdedece16ab774c9d8de8ace4c0a7e23b8f2dca
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-31 00:14:19 +0530

    Update Login.jsx

--- src/Components/SignUP Page/Login.jsx
+++ src/Components/SignUP Page/Login.jsx
@@ -37,7 +37,7 @@ function Login() {
   return (
     <>
       {showAnswer === "" ? (
-        <div className="w-screen h-screen bg-gradient-to-tr from-cyan-300 to-black">
+        <div className="w-screen h-screen bg-gradient-to-tr from-cyan-300 to-gray-600">
           <div className="text-right p-10 text-white">
             <Link to="/">
               <button>
@@ -47,36 +47,35 @@ function Login() {
           </div>
 
           <div className="flex items-center justify-center">
-            <div className="w-full lg:w-4/12 h-auto bg-white rounded-lg">
+            <div className="w-full lg:w-4/12 h-auto bg-transparent rounded-lg shadow-lg shadow-current">
               <div className="m-8 text-5xl text-blue-700 flex flex-col gap-5 items-center">
                 <div>
-                  <h1 className="underline underline-offset-[20px]">Login</h1>
+                  <h1 className="underline underline-offset-[20px] text-white">Login</h1>
                 </div>
-                <div className="under-line"></div>
               </div>
 
               <div className="flex flex-col items-center gap-8 mt-16">
-                <div className="w-11/12 h-14 bg-gray-300 text-centern rounded-md flex items-center">
-                  <span className="text-2xl m-5 lg:m-6 text-gray-400">
+                <div className="w-11/12 h-14 bg-transparent border text-centern rounded-md flex items-center">
+                  <span className="text-2xl m-5 lg:m-6 text-gray-50">
                     <IoMail size={30} />
                   </span>
                   <input
                     type="email"
                     placeholder="Email"
-                    className="h-full w-9/12 lg:w-10/12 bg-transparent text-2xl outline-0"
+                    className="h-full w-9/12 lg:w-10/12 bg-transparent text-2xl outline-0  text-white placeholder:text-gray-50"
                     value={loginValue.email}
                     onChange={(e) => handleChangeLogin(e, "email")}
                   />
                 </div>
 
-                <div className="w-11/12 h-14 bg-gray-300 text-centern rounded-md flex items-center">
-                  <span className="text-2xl m-5 lg:m-6 text-gray-400">
+                <div className="w-11/12 h-14 bg-transparent border text-centern rounded-md flex items-center">
+                  <span className="text-2xl m-5 lg:m-6 text-gray-50">
                     <FaLock size={30} />
                   </span>
                   <input
                     type="password"
                     placeholder="Password"
-                    className="h-full w-9/12 lg:w-10/12 bg-transparent text-2xl outline-0"
+                    className="h-full w-9/12 lg:w-10/12 bg-transparent text-2xl outline-0  text-white placeholder:text-gray-50"
                     value={loginValue.password}
                     onChange={(e) => handleChangeLogin(e, "password")}
                   />
