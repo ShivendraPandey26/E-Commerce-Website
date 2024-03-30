3ca96f1e8e2e9a97e5d395bdd050a65d45c7aa24
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-31 00:15:22 +0530

    Update Card.jsx

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
-const Card = ({ title, price, image, rating, btn, DiscountPercentage }) => {
+const Card = ({ title, price, image, rating, btn, DiscountPercentage, brandName, AddToCartCard}) => {
   return (
     <>
-      <div className="lg:w-64 md:w-64 sm:w-40 w-40 h-[23rem] bg-white shadow-md rounded-lg p-4 hover:scale-110 duration-300 ease-in">
+      <div className="lg:w-64 md:w-64 sm:w-40 w-40 h-[25rem] bg-white shadow-md rounded-lg p-4 hover:scale-110 duration-300 ease-in">
         <img
           src={image}
           alt={title}
           className="w-full h-40 object-cover overflow-hidden rounded-lg mb-4"
         />
         <span className="relative right-4 bottom-[11.8rem] py-2 px-3 bg-red-600 me-10 text-white rounded-md">
-          {DiscountPercentage}%
+          {DiscountPercentage}% 
         </span>
+          <h1>{brandName}</h1>
         <h2 className="lg:text-lg md:text-lg sm:text-sm text-sm font-semibold">
           {title}
         </h2>
         <p className="text-gray-500 text-sm">₹{price}.00</p>
         <p className="text-gray-500 text-sm">Rating : {rating} / 5</p>
-        <button className=" bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 rounded-lg w-full mt-4">
+        <button 
+        className=" bg-orange-500 hover:bg-orange-600 text-white font-semibold py-1 rounded-lg w-full mt-4"
+        onClick={AddToCartCard}
+        >
           {btn}
         </button>
       </div>
