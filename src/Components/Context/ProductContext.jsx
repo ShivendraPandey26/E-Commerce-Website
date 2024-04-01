6c9fe8c03aeed1fc07b87d055ec3d12c6be1dff9
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-04-01 21:18:32 +0530

    Update ProductContext.jsx

--- src/Components/Context/ProductContext.jsx
+++ src/Components/Context/ProductContext.jsx
@@ -4,6 +4,7 @@ import React, { createContext, useContext } from "react";
 export const ProductContext  = createContext({
     AddToCart : ()  => {},
     cartProduct : [],
+    handleQuantity : () => {},
 });
 
 export const ProductContextProvider = ProductContext.Provider;
@@ -11,3 +12,4 @@ export const ProductContextProvider = ProductContext.Provider;
 export default function useProduct() {
     return useContext(ProductContext);
 }
+
