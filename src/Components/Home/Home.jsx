d486d1f18d789bd3ef444f3edfb88365138b7512
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-04-03 23:43:46 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Layout from "../Layout/Layout";
-import ImageSlider from "./ImageSlider";
+// import ImageSlider from "./ImageSlider";
 import FourDiv from "./FourDiv";
 import PopularProducts from "./PopularProducts";
 import Gallery from "./Gallery";
 import Testimonials from "./Testimonials";
-
 
 function Home() {
   return (
@@ -13,7 +12,14 @@ function Home() {
       <Layout>
         {/* Home page */}
         <div className="bg-[#eaf5f5]">
-          <ImageSlider />
+          {/* <ImageSlider /> */}
+          <div>
+            <img
+              src="https://cdn.pixabay.com/photo/2017/03/13/17/26/ecommerce-2140603_640.jpg"
+              alt="header image"
+              className="max-w-full max-h-full w-screen h-[87vh]"
+            />
+          </div>
           <FourDiv />
           <PopularProducts />
           <Gallery />
