478d2a72fe52ce04e8b9d46e1e1db6f05e557a7a
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-04-03 23:43:42 +0530

    Update PopularProducts.jsx

--- src/Components/Home/PopularProducts.jsx
+++ src/Components/Home/PopularProducts.jsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
+import useProduct from "../Context/ProductContext";
+
 
 function PopularProducts() {
+  
+  const [products, setProducts] = useState([]);
+  const { cartProduct, AddToCart } = useProduct();
+
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        let response = await fetch("https://dummyjson.com/products?limit=10&skip=30&select=title,price,thumbnail,rating,discountPercentage,brand,description  ");
+        let data = await response.json();
+        setProducts(data.products);
+        // console.log(data.products);
+      } catch (error) {
+        console.log("Error", error);
+      }
+    };
+
+    getAllProducts();
+  }, []);
+
+
+
   return (
     <div className="p-3">
-      <h2 className="text-4xl font-semibold mb-20 text-[#31587c] font-serif text-center underline underline-offset-8">
-        Popular Products
+      <h2 className="text-4xl font-semibold mb-20 text-[#31587c] font-sans tracking-widest text-center">
+      Best Sellers
       </h2>
 
       <div className="w-screen h-auto lg:p-10 md:p-10 sm:p-3 p-3 flex justify-center items-center lg:gap-10 md:gap-10 sm:gap-2 gap-2 mb-15 flex-wrap ">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+
+        {products.map((item, index) => (
+          <Card
+          key={index}
+          title={item.title}
+          price={item.price}
+          rating={item.rating}
+          image={item.thumbnail}
+          btn={'Add To  Cart'}
+          DiscountPercentage={item.discountPercentage}
+          brandName={item.brand}
+          AddToCartCard ={()=>{AddToCart(item)}}
+        />
+        ))}
+      
+        
       </div>
     </div>
   );
