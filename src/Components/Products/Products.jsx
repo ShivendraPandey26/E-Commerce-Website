71dcff777a73d7b6dc2b1ea5527ba7b144156f4a
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-04-05 00:50:34 +0530

    Update Products.jsx

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,6 +3,7 @@ import Layout from "../Layout/Layout";
 import Card from "../Card/Card";
 import AllProducts from "./AllProducts";
 import useProduct from "../Context/ProductContext";
+import ProductShimEffect from "./ProductShimEffect";
 
 function Products() {
   const [allCategories, setAllCategories] = useState([]);
@@ -10,6 +11,8 @@ function Products() {
   const [productsItems, setProductsItems] = useState([]);
   const [userInput, setUserInput] = useState("");
   const [searchItems, setSearchItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
 
   const { AddToCart } = useProduct();
 
@@ -30,6 +33,7 @@ function Products() {
     setClickProduct(categoryId);
   };
 
+  // function for fetch the clicked  product items
   useEffect(() => {
     const fetchClickProduct = async () => {
       try {
@@ -47,6 +51,7 @@ function Products() {
     fetchClickProduct();
   }, [clickProduct]);
 
+// function for handle the  Search functionality
   const handleSearchUserInput = async () => {
     try {
       if (userInput) {
@@ -57,10 +62,10 @@ function Products() {
         if (data.products) {
           setSearchItems(data.products);
           // console.log(searchItems);
-          setUserInput ("");
+          setUserInput("");
         } else {
           setSearchItems([]);
-          setUserInput ("");
+          setUserInput("");
         }
       }
     } catch (error) {
@@ -68,27 +73,36 @@ function Products() {
     }
   };
 
+// function for loading  the page and getting all products from API
+  useEffect(() => {
+    // Simulate data fetching delay
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 1500)); // Simulated 1.5-second delay
+      setLoading(false); // Once data is fetched, set loading to false
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       <div className="bg-[#eaf5f5]">
-
-
-        <div className="w-full h-20 flex items-center justify-around flex-wrap bg-blue-300 mb-20">
+        <div className="w-full h-20 flex items-center justify-around flex-wrap bg-blue-300 mb-5">
           <div className="ml-10">
             <input
               type="search"
-              className="w-52 h-9 rounded-lg p-5"
+              className="w-52 h-9 rounded-s-lg p-5"
               value={userInput}
               onChange={(e) => setUserInput(e.target.value)}
               onKeyDown={(e) => {
-                if  (e.key === "Enter") {
+                if (e.key === "Enter") {
                   handleSearchUserInput();
-              }
-            }}
+                }
+              }}
             />
             <button
               type="button"
-              className="w-24 h-9 bg-orange-600 text-white text-lg font-semibold rounded-lg hover:bg-orange-700"
+              className="w-24 h-10 bg-orange-600 text-white text-lg font-semibold rounded-e-lg hover:bg-orange-700"
               onClick={handleSearchUserInput}
             >
               Search
@@ -110,26 +124,54 @@ function Products() {
             </select>
           </div>
         </div>
-       
-       <div className="w-screen h-auto flex justify-between">
 
-        {/* Render search results if available */}
-        <div>
-        {searchItems.length > 0 && (
+        <div className="w-screen h-auto flex justify-between">
+          {/* Render search results if available */}
           <div>
-            <h2 className="text-4xl font-semibold mb-10 text-[#31587c] font-sans text-center tracking-widest">
-            Search Products
-            </h2>
-            
-            <div className="w-full h-auto lg:p-10 md:p-10 sm:p-3 p-3 flex justify-center items-center lg:gap-10 md:gap-10 sm:gap-2 gap-2 mb-15 flex-wrap">
-              {searchItems.map((item, index) => (
+            {searchItems.length > 0 && (
+              <div>
+                <h2 className="text-4xl font-semibold mb-10 text-[#31587c] font-sans text-center tracking-widest">
+                  Search Products
+                </h2>
+
+                <div className="w-full h-auto lg:p-10 md:p-10 sm:p-3 p-3 flex justify-center items-center lg:gap-10 md:gap-10 sm:gap-2 gap-2 flex-wrap">
+                  {searchItems.map((item, index) => (
+                    <div key={index}>
+                      <Card
+                        title={item.title}
+                        price={item.price}
+                        rating={item.rating}
+                        image={item.thumbnail}
+                        btn={"Add To Cart"}
+                        DiscountPercentage={item.discountPercentage}
+                        AddToCartCard={() => {
+                          AddToCart(item);
+                        }}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div>
+            <h1
+              className={`text-4xl font-semibold mb-10 text-[#31587c] font-sans text-center tracking-widest capitalize ${
+                clickProduct === "" ? "hidden" : "block"
+              }`}
+            >
+              {clickProduct}
+            </h1>
+            <div className="w-full h-auto lg:p-10 md:p-10 sm:p-3 p-3 flex justify-center items-center lg:gap-10 md:gap-10 sm:gap-2 gap-2 mb-15 flex-wrap ">
+              {productsItems.map((item, index) => (
                 <div key={index}>
                   <Card
                     title={item.title}
                     price={item.price}
                     rating={item.rating}
                     image={item.thumbnail}
-                    btn={"Add To Cart"}
+                    btn={"Add To  Cart"}
                     DiscountPercentage={item.discountPercentage}
                     AddToCartCard={() => {
                       AddToCart(item);
@@ -139,40 +181,14 @@ function Products() {
               ))}
             </div>
           </div>
-        )}
         </div>
-
-
         <div>
-          <h1
-            className={`text-4xl font-semibold mb-10 text-[#31587c] font-sans text-center tracking-widest capitalize ${
-              clickProduct === "" ? "hidden" : "block"
-            }`}
-          >
-            {clickProduct}
-          </h1>
-          <div className="w-full h-auto lg:p-10 md:p-10 sm:p-3 p-3 flex justify-center items-center lg:gap-10 md:gap-10 sm:gap-2 gap-2 mb-15 flex-wrap ">
-            {productsItems.map((item, index) => (
-              <div key={index}>
-                <Card
-                  title={item.title}
-                  price={item.price}
-                  rating={item.rating}
-                  image={item.thumbnail}
-                  btn={"Add To  Cart"}
-                  DiscountPercentage={item.discountPercentage}
-                  AddToCartCard={() => {
-                    AddToCart(item);
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        
-        </div>
-
+      {loading ? (
+        <ProductShimEffect />
+      ) : (
         <AllProducts />
+      )}
+    </div>
       </div>
     </Layout>
   );
