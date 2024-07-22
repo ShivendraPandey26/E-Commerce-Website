af24dcfea765cc902dbb2610ec6e730fb469d74b
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-07-22 17:33:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import Home from "./Components/Home/Home";
-import Products from "./Components/Products/Products";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import SignUP from "./Components/SignUP Page/SignUP";
@@ -12,6 +11,8 @@ import { ProductContextProvider } from "./Components/Context/ProductContext";
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 import SingleProduct from "./Components/SingleProduct/SingleProduct";
+import ProductMain from "./Components/Products/ProductMain"
+
 
 function App() {
   const [cartProduct, setCartProduct] = useState([]);
@@ -39,7 +40,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/home" element={<Home />} />
-            <Route exact path="/products" element={<Products />} />
+            <Route path="/products" element={<ProductMain />} />
             <Route path="/about" element={<About />}></Route>
             <Route path="/contact" element={<Contact />}></Route>
             <Route path="/signup" element={<SignUP />}></Route>
